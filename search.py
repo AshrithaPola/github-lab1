2ffad26eb0239ff53b7126008e9e87077e6efdf2
ashrithapola <polaashritha@gmail.com>
2024-09-17 23:22:41 -0400

    search implementation

--- search.py
+++ search.py
@@ -1,8 +1,23 @@
 import json
 
 def search_json(json_data, search_string):
-    results = []
-    # Place your search code here
-    # you will have to loop through the json_data file you created earlier
-    # finally you can store the match in the result list and return it
+    results = {}
+    
+    # Convert the search string to lower case
+    search_string = search_string.lower()
+
+    for products in json_data:
+        # get the user details
+        user_details = products['User']
+        product = {}
+        
+        for key, value in products.items():
+            if search_string in key.lower():
+               # If the search string is avaliable add the key value pair to the product dictonary 
+               product[key] = value 
+        
+        # Add the user's details and their matched products to the results dictionary    
+        results[user_details] = product
+
+    
     return results
